60f19511a4761e8acbd41e963dcf62952758f415
andrews <andrewInput@gmail.com>
2018-10-30 22:03:03 +0000

    25% speed formula tweaks

--- trigfills.js
+++ trigfills.js
@@ -12,7 +12,7 @@ var trigfillfactory = function(){
   var pi=Math.PI, tau=pi*2 ,hpi=pi/2 ,qpi=pi/4 
      ,spi=pi/6 ,epi=pi/8 ,sq3=1.7320508075688772 //sqrt(3)
 
-  function version(){ return "0.9.3" } 
+  function version(){ return "0.9.4" } 
       
   function modp(a,b){
     return a-Math.floor(a/b)*b //removes neg
@@ -20,19 +20,6 @@ var trigfillfactory = function(){
 
   function modn(a,b){
     return a-Math.floor(a/b + 0.5)*b //creates neg (math.round is slow)
-  }
-                                     
-                                                                               
-  var f24=100000/2399999,f720=100000/71998376,f40k=1000/40578583 
-  function costay(x){
-    var x2=x*x,x4=x2*x2
-    return 1- x2*0.5 + x4*f24 - x2*x4*f720 + x4*x4*f40k 
-  }
-
-  var fh6=100000000/600000005,f120=1/120,f5k=1000/5039680,f362k=10/3628880
-  function sintay(x){
-    var c=x*x*x
-    return x-(c*fh6)+(c*x*x*f120)-(c*c*x*f5k)+(c*c*c*f362k) 
   }
 
   function cos(x){
@@ -56,46 +43,57 @@ var trigfillfactory = function(){
     if(x>qpi) return pol*costay(hpi-x) 
     return pol*sintay(x) 
   }
-  
-  
-  var ff3 =100000000000/299999999177
-     ,ff5 =10000000/50000082
-     ,ff7 =100000/700011 ,ff9 =10000/90019 ,ff11=1000/11768
+                                                                                          
+  var f24=100000/2399999,f720=100000/71998376,f40k=1000/40578583 
+  function costay(x){
+    var x2=x*x,x4=x2*x2
+    return 1 - x2*0.5 + x4*f24 - x4*x2*f720 + x4*x4*f40k 
+  }
+
+  var fh6=100000000/600000005,f120=1/120,f5k=1000/5039680,f362k=10/3628880
+  function sintay(x){
+    var x3=x*x*x,x6=x3*x3
+    return x - x3*fh6 + x3*x*x*f120 - x6*x*f5k + x6*x3*f362k 
+  }
+
+  var ff3=100000000000/299999999177
+     ,ff5=10000000/50000082
+     ,ff7=100000/700011 ,ff9=10000/90019 ,ff11 =1000/11768
   
   function atan(x){
     
-    var pos=1, mut=0, mut2=0
+    var pos=1, mut=false, mut2=false
     
     if(x<0){ pos=-1, x=-x }
-    if(x>1){ x=1/x , mut=1  }
+    if(x>1){ x=1/x , mut=true  }
     if(x>0.26794919){
       x=(sq3*x-1)/(sq3+x) 
-      mut2=1
+      mut2=true
     }
 
-    var x2=x*x ,x4=x2*x2 ,x8=x4*x4
-    x=x -x*x2*ff3 +x4*x*ff5 -x4*x2*x*ff7 +x8*x*ff9 -x8*x2*x*ff11
+    var x2=x*x ,x4=x2*x2 ,x9=x4*x4*x
+    x=x -x*x2*ff3 +x4*x*ff5 -x4*x2*x*ff7 +x9*ff9 -x9*x2*ff11
 
     if(mut2) x+= spi
     if(mut)  x = hpi-x
     return x*pos
   }
                                 
-                                
+   
   var fg3=1000000000/2999999887,f15=200000000/1500000678
      ,f315=170000/3150457,f2k=6200/282874,f155k=1382/146286
   
   function tan(x){
 
-    var pos=1 ,mut=0 ,mut2=0
+    var pos=1 ,mut=false ,mut2=false
     x = modp(x,pi)*0.99999999999999993
 
-    if(x>hpi) pos=-1 ,x= pi-x
-    if(x>qpi) mut= 1 ,x=hpi-x
-    if(x>epi) mut2=2 ,x=x*0.5 
+    if(x>hpi) x= pi-x ,pos=-1 
+    if(x>qpi) x=hpi-x ,mut=true
+    if(x>epi) x=x*0.5 ,mut2=true 
     
-    var c=x*x*x, cc=c*c 
-    x= x +c*fg3 +c*x*x*f15 +cc*x*f315 +cc*c*f2k +cc*c*x*x*f155k
+    var x2=x*x ,x4=x2*x2 ,x9=x4*x4*x
+    x=x +x*x2*fg3 +x4*x*f15 +x4*x2*x*f315 +x9*f2k +x9*x2*f155k
       
     if(mut2) x=2*x/(1-x*x)
     if(mut)  x=1/x 
@@ -105,7 +103,7 @@ var trigfillfactory = function(){
 
   function acos(x){
    if(x==-1) return Math.PI
-   return modp(atan( Math.sqrt(1-x*x)/x),Math.PI )
+   return modp( atan( Math.sqrt(1-x*x)/x),Math.PI )
   }
 
   function asin(x){ return atan( x/Math.sqrt(1-x*x) ) }
